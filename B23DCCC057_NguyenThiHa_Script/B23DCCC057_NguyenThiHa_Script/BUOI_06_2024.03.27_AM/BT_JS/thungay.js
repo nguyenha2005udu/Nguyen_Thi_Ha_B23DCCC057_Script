18c1632121ba972c845ae4923304e1d50791c7af
Nguyễn Thị Hà B23DCCC057 <165117008+Nguynha2005udu@users.noreply.github.com>
2024-04-03 13:10:02 +0700

    Add files via upload

---
+++ B23DCCC057_NguyenThiHa_Script/B23DCCC057_NguyenThiHa_Script/BUOI_06_2024.03.27_AM/BT_JS/thungay.js
@@ -0,0 +1,52 @@
+var a=parseInt(promt("Nhập số: "));
+if(a==2){
+    document.write('Hôm nay là thứ 2');
+}
+else if(a==3){
+    document.write('Hôm nay là thứ 3');
+}
+else if(a==4){
+    document.write('Hôm nay là thứ 4');
+}
+else if(a==5){
+    document.write('Hôm nay là thứ 5');
+}
+else if(a==6){
+    document.write('Hôm nay là thứ 6');
+}
+else if(a==7){
+    document.write('Hôm nay là thứ 7');
+}
+else if(a==8){
+    document.write('Hôm nay là chủ nhật');
+}
+else{
+    document.write('Không hợp lệ');
+}
+
+switch(a){
+    case 2:
+        document.write('Hôm nay là thứ 2');
+        break;
+    case 3:
+        document.write('Hôm nay là thứ 3');
+        break;
+    case 4:
+        document.write('Hôm nay là thứ 4');
+        break;
+    case 5:
+        document.write('Hôm nay là thứ 5');
+        break;
+    case 6:
+        document.write('Hôm nay là thứ 6');
+        break;
+    case 7:
+        document.write('Hôm nay là thứ 7');
+        break;
+    case 8:
+        document.write('Hôm nay là chủ nhật');
+        break;
+    default:
+        document.write('Không hợp lệ');
+        break;
+}
